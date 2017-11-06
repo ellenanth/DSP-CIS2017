c93a50983ad2174f6eb3a55a304ef2edf4bedf14
ellenanth <anthonissen.ellen@gmail.com>
2017-11-06 15:35:25 +0100

    2 options OFDM_mod

--- ofdm_mod_1.m
+++ ofdm_mod_1.m
@@ -1,8 +1,7 @@
 %modulate the given sequence seq to a OFDM sequence
 %P = total number of frames in a packet
-%N = total numbers per frame
 %N_q is used for the QAM modulation to construct the frames
-function [ofdm_seq] = ofdm_mod(seq, P, N_q)    
+function [ofdm_seq] = ofdm_mod_1(seq, P, N_q)    
     %define nb_zeros_ofdm as total number of zeros to add to the original
     %data so the data fits in P frames of size N and the QAM doesn't need
     %padding anymore as length(seq) is a multiple of N_q
@@ -11,7 +10,6 @@ function [ofdm_seq] = ofdm_mod(seq, P, N_q)
     N = 2 * ( ceil( length_seq_qam/P ) + 1 );
     length_seq_ofdm = P * (N/2-1) * N_q;
     nb_zeros_ofdm = length_seq_ofdm - length(seq);
-    %N = 18; %example for length(seq)=64, N_q = 4, P=2 
     
     %padding the original sequence with zeros at the end
     seq = [seq, zeros(1,nb_zeros_ofdm)];
@@ -24,14 +22,19 @@ function [ofdm_seq] = ofdm_mod(seq, P, N_q)
     packet = zeros(N, P);
     end_N = N/2-1;
     for i_P = 1:P
+        %fill in QAM signals, keep zeros on DC and Nyquist frequencies
         start_QAM = (i_P-1) * end_N;
         for i_N = 1:end_N
             packet(i_N+1, i_P) = QAM_seq(start_QAM + i_N); %fill in value from QAM
-            packet(N-i_N+3, i_P) = conj( QAM_seq(start_QAM + i_N) ); %fill in value from QAM*
+            packet(N-i_N+1, i_P) = conj( QAM_seq(start_QAM + i_N) ); %fill in value from QAM*
         end
-        %TODO IFFT per frame
+        
+        %IFFT per frame
+        packet(:,i_P) = ifft(packet(:,i_P));
+        
+        %parallel to serial conversion
+        start_pos = (i_P-1)*N + 1;
+        end_pos = start_pos + N - 1;
+        ofdm_seq(1, start_pos:end_pos) = packet(:,i_P)';
     end
-    disp(packet);
-    
-    %TODO parallel to serial conversion
 end
