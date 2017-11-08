e42cc5653e2267eb4a03806624ee203d47893fd8
ellenanth <anthonissen.ellen@gmail.com>
2017-11-08 13:01:45 +0100

    OZ4

--- ofdm_demod.m
+++ ofdm_demod.m
@@ -1,29 +1,35 @@
 %demodulate the given OFDM sequence seq back to the original sequence
 %N_q is maximaal 6
 %original_length is needed to cut of padded zeros at the end
-function [seq_demod] = ofdm_demod(seq_mod, P, N_q, original_length) 
-    %TODO serial-to-parallel conversion
-    N = length(seq_mod)/P;
-    packet = zeros(N,P);
+%L = length of cyclic prefix
+function [seq_demod] = ofdm_demod(seq_mod, P, N_q, L, original_length) 
+    %serial-to-parallel conversion
+    N = (length(seq_mod))/P - L;
+    packet = zeros(N+L,P);
     QAM_seq = zeros(1, (N/2-1)*P );
 
     for i_P = 1:P
-        %TODO fill frames in packet
-        start_pos = (i_P-1)*N + 1;
-        end_pos = start_pos + N - 1;
+        %fill frames in packet
+        start_pos = (i_P-1)*(N+L) + 1;
+        end_pos = start_pos + (N+L) - 1;
         packet(:,i_P) = seq_mod(1, start_pos:end_pos)';
-        
-        %TODO FFT operation
+    end
+    
+    %cut off cyclic prefix
+    packet = packet((L+1):N+L, :);
+    
+    for i_P = 1:P
+        % FFT operation
         packet(:,i_P) = fft(packet(:,i_P));
         
-        %TODO retrieve QAM sequence
+        % retrieve QAM sequence
         start_QAM = (i_P-1) * (N/2-1) + 1;
         end_QAM = start_QAM + N/2 - 2;
         QAM_seq(1, start_QAM:end_QAM) = transpose( packet(2:(N/2), i_P) ); 
     end
     
 
-    %TODO demodulate QAM sequence
+    % demodulate QAM sequence
     seq_demod = qam_demod(QAM_seq, N_q, original_length); 
     
     %nullen wegdoen om terug orignele lengte te krijgen
--- ofdm_experiment.m
+++ ofdm_experiment.m
@@ -1,12 +1,13 @@
 N = 59; %user defined length of pseudo random binary sequence (PRBS)
 N_q = 4; %2^N_q = constellation size (max. 6)
 P = 2; %number of frames per packet
+L = 10;
 SNR = 3; %signal to noise ratio for the AWGN (additive white gaussian noise)
 
 seq = randi([0,1], 1, N);
 
 %option 1: QAM included in OFDM function
-seq_modulated = ofdm_mod(seq, P, N_q);
-seq_demodulated = ofdm_demod(seq_modulated, P, N_q, N);
+seq_modulated = ofdm_mod(seq, P, N_q, L);
+seq_demodulated = ofdm_demod(seq_modulated, P, N_q, L, N);
 
 BER_cal = ber(seq, seq_demodulated);
